0776c6aeb61ee26db237b32b30996f21a5c924b5
Nikita Domnitskii <nikita@domnitskii.me>
2023-11-16 17:44:55 +0600

    add readme and fix stuff to run app

--- src/backend/phonebook/api/static.clj
+++ src/backend/phonebook/api/static.clj
@@ -13,5 +13,5 @@
      [:title "phonebook"]]
     (hiccup.page/include-css "https://cdn.jsdelivr.net/npm/@picocss/pico@1/css/pico.min.css")
     [:body
-     (hiccup.page/include-js "js/main.js")
-     [:main#app-content.container-fluid]])})
+     [:main#app-content.container-fluid]
+     (hiccup.page/include-js "js/main.js")])})
